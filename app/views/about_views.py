bd7f07a4cef13045fdd2925fce223a35419fbd77
joaonascimentooo <joaonascimento197@gmail.com>
2024-12-09 18:07:56 -0300

    feat: adding images to the main website and episode html

--- app/views/about_views.py
+++ app/views/about_views.py
@@ -2,6 +2,5 @@ from flask import Blueprint, render_template
 
 about = Blueprint('about', __name__)
 
-@about.route('/about')
-def about_page():
-    return render_template('about.html')
+
+
--- app/views/main_views.py
+++ app/views/main_views.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
   return render_template('home.html')
+
+@main.route('/episodes')
+def episodes():
+  return render_template('episodes.html')
